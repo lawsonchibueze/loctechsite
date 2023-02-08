a29aba0a4699be37a43dbd81b5499d2934b50b41
Lawson Chibueze <speak2lawson@gmail.com>
2023-02-08 14:49:30 +0100

    redo backend

--- aws-exports.js
+++ aws-exports.js
@@ -3,10 +3,38 @@
 
 const awsmobile = {
     "aws_project_region": "us-west-2",
-    "aws_appsync_graphqlEndpoint": "https://sfzpf55hivakfnrwxx6qia66ky.appsync-api.us-west-2.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://mc25botkbbhbvgieewp6wttpni.appsync-api.us-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-west-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-42he3trwgna3hodoj6wobgg2ha"
+    "aws_appsync_apiKey": "da2-q2gyni6nhfcuhhsgab75o7oqbu",
+    "aws_cognito_identity_pool_id": "us-west-2:e0ec24f8-f2e3-4153-91db-b27b17a2bde9",
+    "aws_cognito_region": "us-west-2",
+    "aws_user_pools_id": "us-west-2_Nn4zS2lQp",
+    "aws_user_pools_web_client_id": "26vtme2i2hp95qngmj9lhmkfso",
+    "oauth": {},
+    "aws_cognito_username_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": [
+            "REQUIRES_LOWERCASE",
+            "REQUIRES_NUMBERS",
+            "REQUIRES_SYMBOLS",
+            "REQUIRES_UPPERCASE"
+        ]
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ],
+    "aws_user_files_s3_bucket": "loctechsite-storage-f79d8bac111646-staging",
+    "aws_user_files_s3_bucket_region": "us-west-2"
 };
 
 
