7a2ec5cf09b1bcc585505f7c8c2b6e4f19b7bf0c
Lawson Chibueze <speak2lawson@gmail.com>
2023-03-22 15:17:17 +0100

    constru8

--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,6 @@ const Home = () => {
         <h1 className="text-4xl text-black font-bold mb-8">
           Site Under Construction
         </h1>
-        <p className="text-lg mb-2 text-black flex flex-col justify-center items-center text-center">
-          We're working hard to bring you something awesome. Stay tuned!
-        </p>
-
-        <p className="text-lg text-black py-2">
-          Please Call or Send us a Mail:
-        </p>
-
-        <div className="flex flex-col gap-1">
-          <span className="text-violet-700 font-bold text-lg pt-5">
-            +2347038885466
-          </span>
-          <div className="font-semibold">customercare@loctechng.com</div>
-        </div>
       </div>
     </div>
   );
