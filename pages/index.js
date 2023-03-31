57a504f6ee168ca92b16a9ea02c4f59db0c6aa8e
Lawson Chibueze <speak2lawson@gmail.com>
2023-03-31 14:25:17 +0100

    scholarship2

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,40 @@
 import React from "react";
 import { FiFacebook, FiLinkedin, FiTwitter, FiYoutube } from "react-icons/fi";
 import Image from "next/image";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div>
       <div className="bg-neutral-300 h-screen flex flex-col justify-center items-center text-center">
-        <h1 className="text-4xl text-black font-bold mb-8">
-          Site Under Construction
-        </h1>
-        <Image
-          src="/assets/logo1.png"
-          alt="Site under construction"
-          width={200}
-          height={200}
-          className="max-w-lg mb-3 text-black"
-        />
-        <p className="text-lg text-black py-2">follow us on social media</p>
+        <Link href="/https://bit.ly/Scholarship_2023">
+          <h1 className="text-4xl text-black font-bold mb-8 ">
+            Scholarship 2023
+          </h1>
+          <h2 className="text-white bg-violet-700 py-4 font-bold">
+            Click Now to Register for Scholarship 2023
+          </h2>
+          <Image
+            src="/assets/scholar1.jpeg"
+            alt="Site under construction"
+            width={400}
+            height={400}
+            className="max-w-lg mb-3 text-black"
+          />
+          <Link href="/https://bit.ly/Scholarship_2023" className="pt-4 ">
+            <button className="bg-white px-4 rounded-md text-violet-700">
+              Register Now{" "}
+            </button>
+          </Link>
+        </Link>
 
         <div className="flex flex-col gap-1 text-purple-800 font-bold pt-4">
           <span>+234 703 888 5466</span>
           <span className="font-semibold">customercare@loctechng.com</span>
         </div>
+        <p className="text-white bg-violet-700 py-4 font-bold p-6 rounded-lg mt-3">
+          Site is Currently under Maintainance
+        </p>
       </div>
     </div>
   );
